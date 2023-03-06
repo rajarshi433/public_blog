a3ecbe926005d5b66b72b26ab2299bb118c211f4
rajarshicode14 <mannarajarshi65@gmail.com>
2023-03-06 17:28:57 +0530

    Second commit

--- server/index.js
+++ server/index.js
@@ -14,22 +14,13 @@ const followingController = require('./controllers/following.controller.js');
 
 const app = express();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    next();
-});
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-// }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
